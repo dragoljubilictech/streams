db667772951d5810502b265cdea8a6e0b1b57c62
Dragoljub <dragoljubilictech@gmail.com>
2021-12-24 15:17:23 +0100

    Created Header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
+import Header from './Header';
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
       <BrowserRouter>
         <div>
-          <div>
-            <Routes>
-              <Route path="/" element={<StreamList />} />
-              <Route path="/streams/new" element={<StreamCreate />} />
-              <Route path="/streams/edit" element={<StreamEdit />} />
-              <Route path="/streams/delete" element={<StreamDelete />} />
-              <Route path="/streams/show" element={<StreamShow />} />
-            </Routes>
-          </div>
+          <Header />
+          <Routes>
+            <Route path="/" element={<StreamList />} />
+            <Route path="/streams/new" element={<StreamCreate />} />
+            <Route path="/streams/edit" element={<StreamEdit />} />
+            <Route path="/streams/delete" element={<StreamDelete />} />
+            <Route path="/streams/show" element={<StreamShow />} />
+          </Routes>
         </div>
       </BrowserRouter>
     </div>

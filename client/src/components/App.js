c1d49f0344d4cb26af8fc06b7658bd3ddd23956e
Dragoljub <dragoljubilictech@gmail.com>
2021-12-24 11:09:31 +0100

    created streams components and route them

---
+++ client/src/components/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <div>
+            <Routes>
+              <Route path="/" element={<StreamList />} />
+              <Route path="/streams/new" element={<StreamCreate />} />
+              <Route path="/streams/edit" element={<StreamEdit />} />
+              <Route path="/streams/delete" element={<StreamDelete />} />
+              <Route path="/streams/show" element={<StreamShow />} />
+            </Routes>
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
